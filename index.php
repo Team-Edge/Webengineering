0e9a9037823766c1a24a677a1b40c8ae87a4d44a
RoQuoTriX <kevin-mangold@gmx.de>
2017-04-06 12:18:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+
+<head>
+<!DOCTYPE HTML>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+<title> Untervermietung Test</title>
+</head>
+
+<body>
+	<background>
+	<div>
+		<header>
+			<div>
+				<titel>
+				<div>
+					<p><img src='Edge.png' alt='Team Edge'></p>
+				</div>
+				</titel>
+				<login>
+				<div>
+					<table>
+						<tr>
+							<p>
+								<td><label for="username">Benutzername</label></td>
+								<td><input type="text" name="f[username]" id="username" /></td>
+							</p>
+						</tr>
+						<tr>
+							<p>
+								<td><label for="password">Passwort</label></td>
+								<td><input type="password" name="f[password]" id="password" /></td>
+							</p>
+						</tr>
+						<tr>
+							<p>
+								<td><input type="submit" name="submit1" value="Anmelden"></td>
+								<td><input type="submit" name="submit2" value="Registrieren"></td>
+							</p>
+						</tr>
+					</table>
+				</div>
+				</login>
+				<navi>
+				<div>
+					<p> Navigation</p>
+				</div>
+			</div>
+		</header>
+	</div>
+	<div>
+		<content>
+		<div>
+			<p> Content</p>
+		</div>
+		</content>
+	</div>
+	</background>
+</body>
+
+</html>
